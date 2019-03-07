c376322142d8b9252986f4f19238d7f8a698ea02
Harry Chen <i@harrychen.xyz>
2019-03-07 09:17:32 +0800

    Fill old codes to lab3
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- labcodes/lab3/kern/debug/kdebug.c
+++ labcodes/lab3/kern/debug/kdebug.c
@@ -293,7 +293,7 @@ read_eip(void) {
  * */
 void
 print_stackframe(void) {
-     /* LAB1 YOUR CODE : STEP 1 */
+     /* LAB1 2016010981 : STEP 1 */
      /* (1) call read_ebp() to get the value of ebp. the type is (uint32_t);
       * (2) call read_eip() to get the value of eip. the type is (uint32_t);
       * (3) from 0 .. STACKFRAME_DEPTH
@@ -305,5 +305,20 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+
+     uint32_t *ebp = (uint32_t*) read_ebp();
+     uint32_t eip = read_eip();
+
+     int i;
+     for (i = 0; i < STACKFRAME_DEPTH; i++) {
+         cprintf("ebp:0x%08x eip:0x%08x args:", ebp, eip);
+         uint32_t *args = ebp + 2;
+         cprintf("0x%08x 0x%08x 0x%08x 0x%08x\n", args[0], args[1], args[2], args[3]);
+         print_debuginfo(eip - 1);
+         eip = ebp[1];
+         ebp = (uint32_t*) ebp[0];
+         // ebp boundary
+         if (ebp == 0) break;
+     }
 }
 
